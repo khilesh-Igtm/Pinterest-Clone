d6f84c52df1c7267ca2094085a43bb3be0a2b639
khilesh singh <57244134+khilesh-Igtm@users.noreply.github.com>
2022-10-03 12:00:17 +0530

    Update urls.py

--- imagebucket/urls.py
+++ imagebucket/urls.py
@@ -18,11 +18,11 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
+# from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('imageapp.urls')),
-    url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+#     url(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 
 ]+static(
     settings.MEDIA_URL , document_root=settings.MEDIA_ROOT
